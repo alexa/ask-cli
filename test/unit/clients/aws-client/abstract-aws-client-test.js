08508b7193ba39a8a7fb4c01742aa033a6fba5dc
kakha urigashvili <kakuri@amazon.com>
2020-05-06 16:12:46 -0700

    chore: addressing pr comments

--- test/unit/clients/aws-client/abstract-aws-client-test.js
+++ test/unit/clients/aws-client/abstract-aws-client-test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const aws = require('aws-sdk');
 const CONSTANTS = require('@src/utils/constants');
 
-const AbstractClient = require('@src/clients/aws-client/abstract-client');
+const AbstractAwsClient = require('@src/clients/aws-client/abstract-aws-client');
 
 describe('Clients test - abstract client test', () => {
     const TEST_AWS_PROFILE = 'TEST_AWS_PROFILE';
@@ -18,30 +18,32 @@ describe('Clients test - abstract client test', () => {
 
     describe('# constructor tests', () => {
         it('| should set region and credentials profile', () => {
-            const client = new AbstractClient(configuration);
-            expect(client).to.be.instanceof(AbstractClient);
+            const client = new AbstractAwsClient(configuration);
+            expect(client).to.be.instanceof(AbstractAwsClient);
             expect(client.awsRegion).eql(TEST_AWS_REGION);
+            expect(client.awsProfile).eql(TEST_AWS_PROFILE);
             expect(aws.config.region).eql(TEST_AWS_REGION);
             expect(aws.config.credentials.profile).eql(TEST_AWS_PROFILE);
         });
 
         it('| should not set credentials profile since using env variables', () => {
             configuration.awsProfile = CONSTANTS.PLACEHOLDER.ENVIRONMENT_VAR.AWS_CREDENTIALS;
-            const client = new AbstractClient(configuration);
-            expect(client).to.be.instanceof(AbstractClient);
+            const client = new AbstractAwsClient(configuration);
+            expect(client).to.be.instanceof(AbstractAwsClient);
             expect(client.awsRegion).eql(TEST_AWS_REGION);
+            expect(client.awsProfile).eql(CONSTANTS.PLACEHOLDER.ENVIRONMENT_VAR.AWS_CREDENTIALS);
             expect(aws.config.region).eql(TEST_AWS_REGION);
             expect(aws.config.credentials.profile).eql(null);
         });
 
         it('| should throw error when aws profile is not specified ', () => {
             configuration.awsProfile = null;
-            expect(() => new AbstractClient(configuration)).to.throw('Invalid awsProfile or Invalid awsRegion');
+            expect(() => new AbstractAwsClient(configuration)).to.throw('Invalid awsProfile or Invalid awsRegion');
         });
 
         it('| should throw error when aws region is not specified ', () => {
             configuration.awsRegion = null;
-            expect(() => new AbstractClient(configuration)).to.throw('Invalid awsProfile or Invalid awsRegion');
+            expect(() => new AbstractAwsClient(configuration)).to.throw('Invalid awsProfile or Invalid awsRegion');
         });
     });
 });
