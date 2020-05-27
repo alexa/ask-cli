d1bc70f2be55cbab2872742dd291f0293d6c246f
RonWang <nongwang@amazon.com>
2020-05-27 15:57:14 -0700

    feat: support upgrade-project command with un-deployed project

--- test/unit/commands/util/upgrade-project/helper-test.js
+++ test/unit/commands/util/upgrade-project/helper-test.js
@@ -31,6 +31,17 @@ describe('Commands upgrade-project test - helper test', () => {
     const TEST_HANDLER = 'handler';
     const TEST_REVISION_ID = 'revisionId';
     const TEST_ROOT_PATH = 'rootPath';
+    const TSET_SRC_DIR = 'sourceDir';
+    const TEST_HIDDEN_NHS_CONFIG = {
+        deploy_settings: {
+            [TEST_PROFILE]: {
+                skill_id: TEST_SKILL_ID,
+                resources: {
+                    lambda: []
+                }
+            }
+        }
+    };
     const TEST_ARN = 'arn:aws:lambda:us-west-2:123456789012:function:ask-custom-skill-sample-nodejs-fact-default';
     const FIXTURE_RESOURCES_CONFIG_FILE_PATH = path.join(process.cwd(), 'test', 'unit', 'fixture', 'model', 'regular-proj', 'ask-resources.json');
     const FIXTURE_STATES_CONFIG_FILE_PATH = path.join(process.cwd(), 'test', 'unit', 'fixture', 'model', 'regular-proj', '.ask', 'ask-states.json');
@@ -61,36 +72,86 @@ describe('Commands upgrade-project test - helper test', () => {
             revisionId: undefined
         }
     };
+    const TEST_PROTOCOL = 'https';
+    const TEST_HOST = 'git-codecommit.us-west-2.amazonaws.com';
+    const TEST_PATH = 'v1/repos/11111111-2222-3333-4444-555555555555';
+    const formV1Config = (skillId, isHosted, lambdaResources) => {
+        const result = { deploy_settings: {}, alexaHosted: {} };
+        result.deploy_settings[TEST_PROFILE] = {
+            skill_id: skillId,
+            resources: {
+                lambda: lambdaResources
+            }
+        };
+        result.alexaHosted = {
+            isAlexaHostedSkill: isHosted,
+            gitCredentialsCache: {
+                protocol: TEST_PROTOCOL,
+                host: TEST_HOST,
+                path: TEST_PATH
+            }
+        };
+        return result;
+    };
+    const formSkillJson = (topField, apiCustom) => {
+        const result = { [topField]: {} };
+        result[topField].apis = {
+            custom: apiCustom
+        };
+        return result;
+    };
+
+    describe('# test helper method - loadV1ProjConfig', () => {
+        beforeEach(() => {
+            sinon.stub(fs, 'existsSync').returns(true);
+            sinon.stub(fs, 'readJSONSync').returns(TEST_HIDDEN_NHS_CONFIG);
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('| file does not exist, expect throw error', () => {
+            // setup
+            fs.existsSync.returns(false);
+            // call & verify
+            expect(() => helper.loadV1ProjConfig(TEST_ROOT_PATH, TEST_PROFILE)).throw(CLiError,
+                'Failed to find ask-cli v1 project. Please make sure this command is called at the root of the skill project.');
+        });
+
+        it('| profile does not match, expect throw error', () => {
+            // setup
+            const TEST_DIFFERENT_PROFILE = 'different';
+            // call & verify
+            expect(() => helper.loadV1ProjConfig(TEST_ROOT_PATH, TEST_DIFFERENT_PROFILE)).throw(CLiError,
+                `Profile [${TEST_DIFFERENT_PROFILE}] is not configured in the v1 ask-cli's project. \
+Please check ".ask/config" file and run again with the existing profile.`);
+        });
+
+        it('| expect idDeployed=false and v1Config get returned successfully', () => {
+            // call
+            fs.readJSONSync.returns(formV1Config(' ', false, []));
+            const res = helper.loadV1ProjConfig(TEST_ROOT_PATH, TEST_PROFILE);
+            // verify
+            expect(res).deep.equal({
+                isDeployed: false,
+                v1Config: formV1Config(' ', false, [])
+            });
+        });
+
+        it('| expect idDeployed=true and v1Config get returned successfully', () => {
+            // call
+            const res = helper.loadV1ProjConfig(TEST_ROOT_PATH, TEST_PROFILE);
+            // verify
+            expect(res).deep.equal({
+                isDeployed: true,
+                v1Config: TEST_HIDDEN_NHS_CONFIG
+            });
+        });
+    });
 
     describe('# test helper method - extractUpgradeInformation', () => {
-        const TEST_V1_CONFIG_PATH = 'v1ConfigPath';
-        const TEST_PROTOCOL = 'https';
-        const TEST_HOST = 'git-codecommit.us-west-2.amazonaws.com';
-        const TEST_PATH = 'v1/repos/11111111-2222-3333-4444-555555555555';
-        const formV1Config = (skillId, isHosted, lambdaResources) => {
-            const result = { deploy_settings: {}, alexaHosted: {} };
-            result.deploy_settings[TEST_PROFILE] = {
-                skill_id: skillId,
-                resources: {
-                    lambda: lambdaResources
-                }
-            };
-            result.alexaHosted = {
-                isAlexaHostedSkill: isHosted,
-                gitCredentialsCache: {
-                    protocol: TEST_PROTOCOL,
-                    host: TEST_HOST,
-                    path: TEST_PATH
-                }
-            };
-            return result;
-        };
-
         beforeEach(() => {
-            sinon.stub(path, 'join').withArgs(
-                process.cwd(), CONSTANTS.FILE_PATH.ASK_RESOURCES_JSON_CONFIG
-            ).returns(TEST_V1_CONFIG_PATH);
-            path.join.callThrough();
             sinon.stub(fs, 'existsSync').returns(true);
             sinon.stub(fs, 'readJsonSync').returns({});
         });
@@ -99,28 +160,20 @@ describe('Commands upgrade-project test - helper test', () => {
             sinon.restore();
         });
 
-        it('| v1 project file does not exist, expect throw error', () => {
-            // setup
-            fs.existsSync.returns(false);
-            // call & verify
-            expect(() => helper.extractUpgradeInformation(TEST_ROOT_PATH, TEST_PROFILE)).throw(CLiError,
-                'Failed to find ask-cli v1 project. Please make sure this command is called at the root of the skill project.');
-        });
-
         it('| skill ID does not exist, expect throw error message', () => {
             // setup
-            fs.readJsonSync.returns(formV1Config('  ', false, {}));
+            const TEST_CONFIG_OBJ = formV1Config('  ', false, {});
             // call & verify
-            expect(() => helper.extractUpgradeInformation(TEST_ROOT_PATH, TEST_PROFILE)).throw(CLiError,
+            expect(() => helper.extractUpgradeInformation(TEST_CONFIG_OBJ, TEST_PROFILE)).throw(CLiError,
                 `Failed to find skill_id for profile [${TEST_PROFILE}]. \
 If the skill has never been deployed in v1 ask-cli, please start from v2 structure.`);
         });
 
         it('| skill project is alexa hosted skill, expect result correctly', () => {
             // setup
-            fs.readJsonSync.returns(formV1Config(TEST_SKILL_ID, true, null));
+            const TEST_CONFIG_OBJ = formV1Config(TEST_SKILL_ID, true, null);
             // call
-            const info = helper.extractUpgradeInformation(TEST_ROOT_PATH, TEST_PROFILE);
+            const info = helper.extractUpgradeInformation(TEST_CONFIG_OBJ, TEST_PROFILE);
             // verify
             expect(info).deep.equal({
                 skillId: TEST_SKILL_ID,
@@ -174,9 +227,9 @@ If the skill has never been deployed in v1 ask-cli, please start from v2 structu
             ].forEach(({ testCase, lambdas, expectError }) => {
                 it(testCase, () => {
                     // setup
-                    fs.readJsonSync.returns(formV1Config(TEST_SKILL_ID, false, lambdas));
+                    const TEST_CONFIG_OBJ = formV1Config(TEST_SKILL_ID, false, lambdas);
                     // call & verify
-                    expect(() => helper.extractUpgradeInformation(TEST_ROOT_PATH, TEST_PROFILE)).throw(CLiError, expectError);
+                    expect(() => helper.extractUpgradeInformation(TEST_CONFIG_OBJ, TEST_PROFILE)).throw(CLiError, expectError);
                 });
             });
         });
@@ -193,9 +246,9 @@ If the skill has never been deployed in v1 ask-cli, please start from v2 structu
 
             it('| when no Lambda ARN exists, skip the upgrade with a warning message', () => {
                 // setup
-                fs.readJsonSync.returns(formV1Config(TEST_SKILL_ID, false, TEST_LAMBDAS));
+                const TEST_CONFIG_OBJ = formV1Config(TEST_SKILL_ID, false, TEST_LAMBDAS);
                 // call
-                const info = helper.extractUpgradeInformation(TEST_ROOT_PATH, TEST_PROFILE);
+                const info = helper.extractUpgradeInformation(TEST_CONFIG_OBJ, TEST_PROFILE);
                 // verify
                 expect(warnStub.args[0][0]).equal('Skip Lambda resource with alexaUsage "custom/default" since this Lambda is not deployed.');
                 expect(info).deep.equal({
@@ -232,9 +285,9 @@ If the skill has never been deployed in v1 ask-cli, please start from v2 structu
                         revisionId: TEST_REVISION_ID
                     }
                 ];
-                fs.readJsonSync.returns(formV1Config(TEST_SKILL_ID, false, TEST_LAMBDAS_MULTIPLE_CODEBASE));
+                const TEST_CONFIG_OBJ = formV1Config(TEST_SKILL_ID, false, TEST_LAMBDAS_MULTIPLE_CODEBASE);
                 // call
-                const info = helper.extractUpgradeInformation(TEST_ROOT_PATH, TEST_PROFILE);
+                const info = helper.extractUpgradeInformation(TEST_CONFIG_OBJ, TEST_PROFILE);
                 // verify
                 expect(warnStub.args[0][0]).equal(`Currently ask-cli requires one Lambda codebase per region. \
 You have multiple Lambda codebases for region ${TEST_REGION}, we will use "${TEST_CODE_URI}" as the codebase for this region.`);
@@ -361,7 +414,7 @@ You have multiple Lambda codebases for region ${TEST_REGION}, we will use "${TES
             sinon.stub(SkillMetadataController.prototype, 'getSkillPackage').callsArgWith(3, TEST_ERROR);
             // call
             helper.downloadSkillPackage(TEST_ROOT_PATH, TEST_SKILL_ID, TEST_SKILL_STAGE, TEST_PROFILE, TEST_DO_DEBUG, (err) => {
-                expect(err).equal(TEST_ERROR);
+                expect(err.includes(TEST_ERROR)).equal(true);
                 done();
             });
         });
@@ -497,6 +550,135 @@ You have multiple Lambda codebases for region ${TEST_REGION}, we will use "${TES
             expect(ResourcesConfig.getInstance().getSkillInfraDeployState(TEST_PROFILE)[TEST_PROFILE].lambda.arn).deep.equal(TEST_ARN);
             expect(ResourcesConfig.getInstance().getSkillInfraUserConfig(TEST_PROFILE)).deep.equal(TEST_CONFIG);
             expect(ResourcesConfig.prototype.write.callCount).to.equal(1);
+        });
+    });
+
+    describe('# test helper method - attemptUpgradeUndeployedProject', () => {
+        beforeEach(() => {
+            sinon.stub(fs, 'existsSync');
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST)).returns(true);
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, 'models')).returns(true);
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, 'isps')).returns(true);
+            fs.existsSync.callThrough();
+            sinon.stub(fs, 'readJSONSync').returns(formSkillJson('manifest', { endpoint: TEST_ARN }));
+            sinon.stub(fs, 'removeSync');
+            sinon.stub(fs, 'writeJSONSync');
+            sinon.stub(fs, 'mkdirpSync');
+            sinon.stub(fs, 'moveSync');
+            sinon.stub(awsUtil, 'getAWSProfile').returns('doesnt matter');
+            sinon.stub(awsUtil, 'getCLICompatibleDefaultRegion').returns(TEST_AWS_REGION);
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('| skill.json file does not exist, expect throw error message', () => {
+            // setup
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST)).returns(false);
+            // call
+            try {
+                helper.attemptUpgradeUndeployedProject(TEST_ROOT_PATH, TEST_HIDDEN_NHS_CONFIG, TEST_PROFILE);
+            } catch (e) {
+                // verify
+                expect(e.message).equal('Unable to upgrade the project. skill.json file must exist.');
+            }
+        });
+
+        it('| skill.json file does not exist, expect throw error message', () => {
+            // setup
+            fs.readJSONSync.returns(formSkillJson('skillManifest', {}));
+            // call
+            try {
+                helper.attemptUpgradeUndeployedProject(TEST_ROOT_PATH, TEST_HIDDEN_NHS_CONFIG, TEST_PROFILE);
+            } catch (e) {
+                // verify
+                expect(e.message).equal('Invalid v1 project without "apis.custom.endpoint" field set in skill.json.');
+            }
+        });
+
+        it('| v1Config does not have lambdaResources, models and isps folder do not exist, expect upgrade succeeds', () => {
+            // setup
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, 'models')).returns(false);
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, 'isps')).returns(false);
+            const NO_LAMBDA_V1CONFIG = formV1Config(TEST_SKILL_ID, false, null);
+            // call
+            helper.attemptUpgradeUndeployedProject(TEST_ROOT_PATH, NO_LAMBDA_V1CONFIG, TEST_PROFILE);
+            // verify
+            expect(fs.removeSync.args[0][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.HIDDEN_ASK_FOLDER));
+            expect(fs.removeSync.args[1][0]).equal(path.join(TEST_ROOT_PATH, 'hooks'));
+            expect(fs.removeSync.args[2][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST));
+            expect(fs.writeJSONSync.args[0][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.ASK_RESOURCES_JSON_CONFIG));
+            expect(fs.writeJSONSync.args[1][0]).equal(
+                path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.PACKAGE, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST)
+            );
+            expect(fs.moveSync.callCount).equal(0);
+        });
+
+        it('| v1Config has lambdaResources without custom/default, models and isps folder do not exist, expect upgrade succeeds', () => {
+            // setup
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, 'models')).returns(false);
+            fs.existsSync.withArgs(path.join(TEST_ROOT_PATH, 'isps')).returns(false);
+            const NO_LAMBDA_V1CONFIG = formV1Config(TEST_SKILL_ID, false, [{ alexaUsage: 'custom/NA' }]);
+            // call
+            helper.attemptUpgradeUndeployedProject(TEST_ROOT_PATH, NO_LAMBDA_V1CONFIG, TEST_PROFILE);
+            // verify
+            expect(fs.removeSync.args[0][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.HIDDEN_ASK_FOLDER));
+            expect(fs.removeSync.args[1][0]).equal(path.join(TEST_ROOT_PATH, 'hooks'));
+            expect(fs.removeSync.args[2][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST));
+            expect(fs.writeJSONSync.args[0][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.ASK_RESOURCES_JSON_CONFIG));
+            expect(fs.writeJSONSync.args[1][0]).equal(
+                path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.PACKAGE, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST)
+            );
+            expect(fs.moveSync.callCount).equal(0);
+        });
+
+        it('| v1Config has lambdaResources, models and isps folder exist, expect upgrade succeeds', () => {
+            // setup
+            fs.readJSONSync.returns(formSkillJson('skillManifest', {
+                endpoint: {
+                    sourceDir: TSET_SRC_DIR,
+                    uri: TEST_ARN
+                }
+            }));
+            const WITH_LAMBDA_V1CONFIG = formV1Config(TEST_SKILL_ID, false, TEST_LAMBDAS);
+            // call
+            helper.attemptUpgradeUndeployedProject(TEST_ROOT_PATH, WITH_LAMBDA_V1CONFIG, TEST_PROFILE);
+            // verify
+            expect(fs.removeSync.args[0][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.HIDDEN_ASK_FOLDER));
+            expect(fs.removeSync.args[1][0]).equal(path.join(TEST_ROOT_PATH, 'hooks'));
+            expect(fs.removeSync.args[2][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST));
+            expect(fs.writeJSONSync.args[0][0]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.ASK_RESOURCES_JSON_CONFIG));
+            expect(fs.writeJSONSync.args[0][1].profiles[TEST_PROFILE]).deep.equal({
+                skillMetadata: {
+                    src: `./${CONSTANTS.FILE_PATH.SKILL_PACKAGE.PACKAGE}`
+                },
+                code: {
+                    default: {
+                        src: TSET_SRC_DIR
+                    }
+                },
+                skillInfrastructure: {
+                    type: CONSTANTS.DEPLOYER_TYPE.LAMBDA.NAME,
+                    userConfig: {
+                        awsRegion: TEST_AWS_REGION,
+                        runtime: TEST_RUNTIME,
+                        handler: TEST_HANDLER
+                    }
+                }
+            });
+            expect(fs.writeJSONSync.args[1][0]).equal(
+                path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.PACKAGE, CONSTANTS.FILE_PATH.SKILL_PACKAGE.MANIFEST)
+            );
+            expect(fs.writeJSONSync.args[1][1]).deep.equal({
+                manifest: {
+                    apis: {
+                        custom: {}
+                    }
+                }
+            });
+            expect(fs.moveSync.args[0][1]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.PACKAGE, 'interactionModels', 'custom'));
+            expect(fs.moveSync.args[1][1]).equal(path.join(TEST_ROOT_PATH, CONSTANTS.FILE_PATH.SKILL_PACKAGE.PACKAGE, 'isps'));
         });
     });
 });
