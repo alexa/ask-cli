999410b20cbeb38613044d99f08720d135beed22
RonWang <nongwang@amazon.com>
2020-11-04 15:59:26 -0800

    feat: implement "ask skill add-locales" command

--- bin/ask-util.js
+++ bin/ask-util.js
@@ -9,4 +9,5 @@ if (!process.argv.slice(2).length) {
     utilCommander.commander.outputHelp();
 } else if (Object.keys(utilCommander.UTIL_COMMAND_MAP).indexOf(process.argv[2]) === -1) {
     console.error('Command not recognized. Please run "ask util" to check the user instructions.');
+    process.exit(1);
 }
---
+++ test/unit/commands/skill/add-locales/helper-test.js
@@ -0,0 +1,195 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const fs = require('fs-extra');
+const path = require('path');
+
+const httpClient = require('@src/clients/http-client');
+const SkillMetadataController = require('@src/controllers/skill-metadata-controller');
+const helper = require('@src/commands/skill/add-locales/helper');
+const Manifest = require('@src/model/manifest');
+const ResourcesConfig = require('@src/model/resources-config');
+const CONSTANTS = require('@src/utils/constants');
+const stringUtils = require('@src/utils/string-utils');
+
+describe('Commands add-locales - helper test', () => {
+    const FIXTURE_RESOURCES_CONFIG_FILE_PATH = path.join(process.cwd(), 'test', 'unit', 'fixture', 'model', 'regular-proj', 'ask-resources.json');
+    const FIXTURE_MANIFEST_FILE_PATH = path.join(process.cwd(), 'test', 'unit', 'fixture', 'model', 'manifest.json');
+    const TEST_PROFILE = 'profile';
+    const TEST_DEBUG = false;
+
+    describe('# unit test for method initiateModels', () => {
+        beforeEach(() => {
+            sinon.stub(path, 'join')
+                .onFirstCall()
+                .returns(FIXTURE_RESOURCES_CONFIG_FILE_PATH)
+                .onSecondCall()
+                .returns(FIXTURE_MANIFEST_FILE_PATH);
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('| skill package source is not set, expect throw error', () => {
+            // setup
+            sinon.stub(stringUtils, 'isNonBlankString').returns(false);
+            // call & verify
+            expect(() => helper.initiateModels(TEST_PROFILE)).to.throw('Skill package src is not found in ask-resources.json.');
+        });
+
+        it('| skill package source does not exist, expect throw error', () => {
+            // setup
+            sinon.stub(stringUtils, 'isNonBlankString').returns(true);
+            sinon.stub(fs, 'existsSync').returns(false);
+            // call & verify
+            expect(() => helper.initiateModels(TEST_PROFILE)).to.throw(/The skillMetadata src file/);
+        });
+
+        it('| project model intiated successfully', () => {
+            // setup
+            sinon.stub(stringUtils, 'isNonBlankString').returns(true);
+            sinon.stub(fs, 'existsSync').returns(true);
+            // call
+            expect(() => helper.initiateModels(TEST_PROFILE)).not.to.throw();
+        });
+    });
+
+    describe('# unit test for method addLocales', () => {
+        const TEST_ERROR = 'error';
+        const TEST_MODEL_PATH = 'filepath';
+        const TEST_SKILL_META_SRC = 'skillmetasrc';
+        const TEST_PATH = 'pathdoesnotmatter';
+        const TEST_TEMPLATE_BODY = { body: 'template' };
+        const TEST_TEMPLATE_MAP = {
+            statusCode: 200,
+            body: {
+                INTERACTION_MODEL_BY_LANGUAGE: {
+                    es: 'TEST_ES_URL',
+                    en: 'TEST_EN_URL'
+                }
+            }
+        };
+        new ResourcesConfig(FIXTURE_RESOURCES_CONFIG_FILE_PATH);
+        new Manifest(FIXTURE_MANIFEST_FILE_PATH);
+
+        beforeEach(() => {
+            sinon.stub(path, 'join').returns(TEST_PATH);
+            sinon.stub(httpClient, 'request');
+            sinon.stub(SkillMetadataController.prototype, 'getInteractionModelLocales');
+            sinon.stub(ResourcesConfig.prototype, 'getSkillMetaSrc').returns(TEST_SKILL_META_SRC);
+            sinon.stub(fs, 'existsSync');
+            sinon.stub(fs, 'copySync');
+            sinon.stub(fs, 'writeFileSync');
+            sinon.stub(Manifest.prototype, 'write');
+            sinon.stub(Manifest.prototype, 'getPublishingLocale').returns({ dummy: 'dummy' });
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('| fail to get the template map with http error', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, TEST_ERROR);
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(err).equal('Failed to retrieve the template list. Please run again with --debug to check more details.');
+                expect(result).equal(undefined);
+                done();
+            });
+        });
+
+        it('| fail to get the template map with statusCode not existing', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, {});
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(err).equal('Failed to retrieve the template list. Please run again with --debug to check more details.');
+                expect(result).equal(undefined);
+                done();
+            });
+        });
+
+        it('| fail to get the template map with statusCode not correct', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, { statusCode: 401 });
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(err).equal('Failed to retrieve the template list. Please run again with --debug to check more details.');
+                expect(result).equal(undefined);
+                done();
+            });
+        });
+
+        it('| selected locale is same as local file', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, TEST_TEMPLATE_MAP);
+            SkillMetadataController.prototype.getInteractionModelLocales.returns({ 'en-US': TEST_MODEL_PATH });
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(err).equal(undefined);
+                expect(result.size).equal(0);
+                done();
+            });
+        });
+
+        it('| selected locale has same language as the local file', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, TEST_TEMPLATE_MAP);
+            SkillMetadataController.prototype.getInteractionModelLocales.returns({ 'en-XX': TEST_MODEL_PATH });
+            fs.existsSync.returns(true);
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(fs.copySync.args[0][0]).equal(TEST_MODEL_PATH);
+                expect(err).equal(undefined);
+                expect(result.size).equal(1);
+                expect(result.get('en-US')).equal(TEST_MODEL_PATH);
+                done();
+            });
+        });
+
+        it('| interaction model for the selected locale can only be provided by official template, http fails', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, TEST_TEMPLATE_MAP);
+            SkillMetadataController.prototype.getInteractionModelLocales.returns({ 'es-US': TEST_MODEL_PATH });
+            fs.existsSync.returns(false);
+            httpClient.request.onSecondCall().callsArgWith(3, TEST_ERROR);
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(err).equal(`Failed to get TEST_EN_URL. ${TEST_ERROR}`);
+                expect(result).equal(undefined);
+                done();
+            });
+        });
+
+        it('| interaction model for the selected locale can only be provided by official template, http returns with 4xx', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, TEST_TEMPLATE_MAP);
+            SkillMetadataController.prototype.getInteractionModelLocales.returns({ 'es-US': TEST_MODEL_PATH });
+            fs.existsSync.returns(false);
+            httpClient.request.onSecondCall().callsArgWith(3, null, { statusCode: 401 });
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(err).equal('Failed to get TEST_EN_URL. Http status code: 401');
+                expect(result).equal(undefined);
+                done();
+            });
+        });
+
+        it('| interaction model for the selected locale can only be provided by official template, http request passes', (done) => {
+            // setup
+            httpClient.request.onFirstCall().callsArgWith(3, undefined, TEST_TEMPLATE_MAP);
+            SkillMetadataController.prototype.getInteractionModelLocales.returns({ 'es-US': TEST_MODEL_PATH });
+            fs.existsSync.returns(false);
+            httpClient.request.onSecondCall().callsArgWith(3, null, TEST_TEMPLATE_BODY);
+            // call
+            helper.addLocales(['en-US'], TEST_PROFILE, TEST_DEBUG, (err, result) => {
+                expect(fs.writeFileSync.args[0][1]).equal(TEST_TEMPLATE_BODY.body);
+                expect(result.size).equal(1);
+                expect(result.get('en-US')).equal('TEST_EN_URL');
+                expect(err).equal(undefined);
+                done();
+            });
+        });
+    });
+});
