e2eb0c0a8297f93e80e419dcfba2c1fc68a5cda5
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-01-15 15:41:23 +0200

    fix: maven java it project should describe the assembly plugin

--- test/unit/builtins/build-flows/java-mvn-test.js
+++ test/unit/builtins/build-flows/java-mvn-test.js
@@ -31,8 +31,7 @@ describe('JavaMvnBuildFlow test', () => {
             buildFlow.execute((err, res) => {
                 expect(err).eql(undefined);
                 expect(res).eql(undefined);
-                expect(execStub.args[0][0]).eql('mvn clean package org.apache.maven.plugins:maven-'
-                + 'assembly-plugin:3.3.0:single -DdescriptorId=jar-with-dependencies');
+                expect(execStub.args[0][0]).eql('mvn clean package org.apache.maven.plugins:maven-assembly-plugin:single');
                 done();
             });
         });
@@ -44,8 +43,7 @@ describe('JavaMvnBuildFlow test', () => {
             buildFlow.execute((err, res) => {
                 expect(err).eql(undefined);
                 expect(res).eql(undefined);
-                expect(execStub.args[0][0]).eql('mvn clean package org.apache.maven.plugins:maven-'
-                + 'assembly-plugin:3.3.0:single -DdescriptorId=jar-with-dependencies');
+                expect(execStub.args[0][0]).eql('mvn clean package org.apache.maven.plugins:maven-assembly-plugin:single');
                 expect(debugStub.args[0][0]).eql('Building skill artifacts based on the pom.xml.');
                 expect(debugStub.args[1][0]).eql('Renaming the jar file some-path to buildFile.');
                 done();
